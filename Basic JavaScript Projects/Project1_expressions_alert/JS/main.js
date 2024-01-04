e477576e80bd38dbd31931cd07eb45a5118539a3
Kari Fuller <kari.christine.fuller@gmail.com>
2024-01-04 06:07:42 -0800

    Added Basic JavaScript Project Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var firstName = 'Kari' //assigning value to var firstName//
+document.write(firstName); //writing firstName value//
+
+var hello = 'Hello there!' //selecting what the alert window will say//
+window.alert(hello); //creating the alert window//
+
+var Sent1 = 'This is the beginning of the string' //first part of the sentence to concatenate//
+var Sent2 = ' and this is the end of the string' //second part to concatenate with//
+document.write('This is the beginning of the string' + ' and this is the end of the string'); //print sentence
+
+document.write(5 + 5) //expression to return 5+5//
+
+
+
+
