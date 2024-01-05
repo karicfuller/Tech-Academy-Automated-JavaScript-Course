dcdefe05915f5c34f8bde8a95eb7767c11d89df2
Kari Fuller <kari.christine.fuller@gmail.com>
2024-01-04 18:24:36 -0800

    Math Operators Complete

--- Basic JavaScript Projects/Project3_math-operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math-operators/JS/main.js
@@ -2,3 +2,23 @@ function aFunction() {
     var afterClick = 'I hate math!';
     document.getElementById('buttonText').innerHTML = afterClick;
 }
+
+function subtract() {
+    var subtraction = 5 - 2;
+    document.getElementById('math').innerHTML = '5 - 2 = ' + subtraction;
+}
+
+function multiply() {
+    var mult = 2 * 2;
+    document.getElementById('theMath').innerHTML = '2 * 2 = ' + mult;
+}
+
+function divide() {
+    var div = 2 / 2;
+    document.getElementById('divMath').innerHTML = '2 / 2 = ' + div;
+}
+
+function addition() {
+    var add = 2 + 2;
+    document.getElementById('addMath').innerHTML = '2 + 2 = ' + add;
+}
